30ac35f4b2a731bb97d062ac34cf80a6d8d88421
OcelusPRO <ocelus1999@hotmail.com>
2022-12-10 16:17:59 +0100

    ✨ Introduce new features. -- Delete event button

---
+++ src/main/kotlin/fr/ftnl/grupo/core/commands/list/buttons/DeleteEvent.kt
@@ -0,0 +1,25 @@
+package fr.ftnl.grupo.core.commands.list.buttons
+
+import fr.ftnl.grupo.core.commands.IButtonCmd
+import fr.ftnl.grupo.database.models.MatchmakingEvent
+import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
+
+class DeleteEvent : IButtonCmd {
+    override suspend fun action(event: ButtonInteractionEvent) {
+        val eventId = event.componentId.split("::")[1].toInt()
+        val mEvent = MatchmakingEvent.cache.get(eventId)
+            ?: return event.reply("L'événement n'existe plus").setEphemeral(true).queue()
+        event.editComponents(event.message.components.map { it.asDisabled() }).queue()
+        
+    }
+    
+    override val name: String
+        get() = "MATCHMAKING_CANCEL::"
+    override val userPermissions: List<Permission>
+        get() = listOf(
+            Permission.MANAGE_EVENTS
+        )
+    override val allowDM: Boolean
+        get() = false
+}
